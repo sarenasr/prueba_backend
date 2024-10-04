29ed779e5d2515edebc4612bd27f239b00ad78c4
s.arensr <s.arenasr@uniandes.edu.co>
2024-10-04 16:11:29 -0500

    GET requests no persistence

--- app.py
+++ app.py
@@ -1,11 +1,86 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+import requests
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pokemon.db' #Persistencia de datos
+db = SQLAlchemy(app)
 
+
+#Crear la entidad de pokemones con sus respectivos parametros
+class Pokemon(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    pokedex_number = db.Column(db.Integer, nullable=False)
+    abilities = db.Column(db.String(200))
+    sprites = db.Column(db.String(200))
+    types = db.Column(db.String(100))
+
+#Crear la base de datos para la persistencia
+@app.before_request
+def create_tables():
+    db.create_all()
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
+
+"""
+Ruta del API general
+"""
+@app.route('/api/pokemon', methods=['GET'])
+def general():
+    response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=20')
+    data = response.json()
+    results = [{"name": pokemon["name"], "url": pokemon["url"]} for pokemon in data['results']]
+    return jsonify(results)
+
+"""
+Ruta del API especifico con posibilidad de modificacion
+"""
+@app.route('/api/pokemon/<id_or_name>', methods=['GET','PUT'])
+def specific(id_or_name):
+    #Pokemon a encontrar
+    pokemon = Pokemon.query.filter_by(name=id_or_name).first() or Pokemon.query.filter_by(pokedex_number=id_or_name).first()
+
+    if request.method == 'GET':
+        #Revisar si el pokemon esta en la DB local
+        if not pokemon:
+            response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{id_or_name}')
+            if response.status_code == 200:
+                data = response.json()
+                return jsonify({
+                    "nombre": data['name'],
+                    "habilidades": [ability['ability']['name'] for ability in data['abilities']],
+                    "pokedex": data['id'],
+                    "sprites": data['sprites']['front_default'],
+                    "tipo": [t['type']['name'] for t in data['types']]
+                })
+        #Si esta en la DB local
+        else:
+            # Return desde local DB
+            return jsonify({
+                "nombre": pokemon.name,
+                "habilidades": pokemon.abilities.split(','),
+                "pokedex": pokemon.pokedex_number,
+                "sprites": pokemon.sprites,
+                "tipo": pokemon.types.split(',')
+            })
+    elif request.method == 'PUT':
+        if not pokemon:
+            return jsonify({"error":"Pokemon no encontrado"})
+
+        data = request.json
+        pokemon.name = data.get('nombre',pokemon.name)
+        pokemon.abilities = ','.join(data.get('habilidades', []))
+        pokemon.sprites = data.get('sprites', pokemon.sprites)
+        pokemon.types = ','.join(data.get('tipo', []))
+        db.session.commit()
+        return jsonify({"message": "Pokémon modificado correctamente"}), 200
+
+
+
 
 
 if __name__ == '__main__':
